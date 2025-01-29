9762ae395a4302952e8bf050b730a20ac232f90f
swagatknaik148 <swagatnaik08@gmail.com>
2025-01-29 21:12:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("Hello Word");
+
+console.log("Code is running..")
+console.log("Code is also running..")
+console.log("Code is also also running..")
+
+var a = prompt("Enter your number")
+var istrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if(istrue){
+    console.log("computer is blastin")
+}
+
+else{
+    console.log("computer is not blastin")
+}
+
+console.log("Your number is " + a)
